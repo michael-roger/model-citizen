34fa8073b3c893dc044e76f3ccf6b191ebc63da5
Michael Roger <michael.roger@playonsports.com>
2025-04-06 17:57:52 -0400

    Initial commit

--- routes/trainers.py
+++ routes/trainers.py
@@ -66,12 +66,16 @@ def edit(trainer_id):
             "SELECT trainer_specialty_id FROM trainer_trainer_specialty_mappings WHERE trainer_id = :trainer"
         ), {"trainer": trainer_id}).mappings()}
     if request.method == 'POST':
+        # Helper function to convert empty strings to None
+        def empty_to_none(value):
+            return value if value else None
+
         data = {
             "id": trainer_id,
             "first_name": request.form.get('first_name', ''),
             "last_name": request.form.get('last_name', ''),
             "date_of_hire": request.form.get('date_of_hire', None),
-            "date_of_termination": request.form.get('date_of_termination', None),
+            "date_of_termination": empty_to_none(request.form.get('date_of_termination')),
             "date_of_birth": request.form.get('date_of_birth', None),
             "job_title": request.form.get('job_title', ''),
             "photo_url": request.form.get('photo_url', ''),
